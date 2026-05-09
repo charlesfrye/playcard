61825521b196672a6951331a692cbd5a3a4fbb01
Charles Frye <cfrye59@gmail.com>
2026-05-09 13:28:10 -0700

    mvp frontend

---
+++ playcard-frontend/serve.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+import modal
+
+app = modal.App("playcard-v0")
+
+__here__ = Path(__file__).parent
+
+image = modal.Image.debian_slim().add_local_file(
+    __here__ / "index.html", "/root/index.html"
+)
+PORT = 8000
+
+
+@app.function(image=image)
+@modal.concurrent(max_inputs=100)
+@modal.web_server(port=PORT)
+def serve():
+    import subprocess
+
+    subprocess.run(["touch", "index.html"])
+    subprocess.Popen(["python", "-m", "http.server", f"{PORT}"])
